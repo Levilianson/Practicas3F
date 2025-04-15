70ebbb75293212c3d90ce19b115e05bb32142ba0
Levilianson <omaribarra27@gmail.com>
2025-04-15 00:47:22 -0300

    Listo los 5 de la clase 3

--- ejerClase03/01Ejer03.py
+++ ejerClase03/01Ejer03.py
@@ -10,7 +10,8 @@
       D
    Calcular el promedio de una lista de números usando args y un operador ternario
    Imprimir un mensaje de error si no se pasan suficientes argumentos
-
+      F
+   Imprimir un mensaje de error si no se pasan suficientes argumentos
 
 """
 def esPar(valor1):
@@ -28,22 +29,38 @@ def buscarPalabra(busqueda, *parrafo):
    palabraBuscada = busqueda in parrafo
    return palabraBuscada 
 
+def cantArgumentos(*args):
+   try:
+      if len(args) < 2:
+         raise ValueError("No se han pasado suficientes argumentos.")
+      else:
+         return ("argumentos suficientes")
+   except ValueError as e:
+      print(f"Error: {e}")
+      return None
+      
+
 def main():
    # A hay que colocar los verificadores si son numeros
-   #dato1=int(input("Ingresar primer numero = "))
-   # dato2=int(input("Ingresar segundo numero = "))   
-   # print(f"el mayor es = " , mayor(dato1, dato2))
-   # # B
-   # palabras=input("Ingrese frase: ")
-   # aBuscar= input("Palabra de busqueda : ")
-   # creo una lista que es iterable con el separador de espacio
-   # frase = palabras.split(" ")
-   # print(f"no buscas nada" if aBuscar =="" else aBuscar + " esta en " + palabras 
-   #       if buscarPalabra(aBuscar, *frase)else aBuscar +" no esta en "+palabras)
-   # # C
-   # print(f"El numero {dato1} es "+ esPar(dato1))     
+   dato1=int(input("Ingresar primer numero = "))
+   dato2=int(input("Ingresar segundo numero = "))   
+   print(f"el mayor es = " , mayor(dato1, dato2))
+   # B
+   palabras=input("Ingrese frase: ")
+   aBuscar= input("Palabra de busqueda : ")
+      # creo una lista que es iterable con el separador de espacio
+   frase = palabras.split(" ")
+   print(f"no buscas nada" if aBuscar =="" else aBuscar + " esta en " + palabras 
+         if buscarPalabra(aBuscar, *frase)else aBuscar +" no esta en "+palabras)
+   # C
+   print(f"El numero {dato1} es "+ esPar(dato1))     
    # D
    valores={6,5,8,9,1,0,4,6}
    #valores={9}     
    print(f"el promedio de {valores} es {promedio(*valores)}" if len(valores)>1 else "faltan numeros")
+   # F Hay tres grupos para probar
+   #argumentos={}
+   argumentos={9}
+   #argumentos=(1, "hola")
+   print (f"Estos {argumentos} son pocos" if cantArgumentos(*argumentos)==None else "son mas de 1")
 main()
